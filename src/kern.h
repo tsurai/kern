8557fec8500dcd23d928955d639c94bd6808907d
Cristian Kubis <cristian.kubis@tsunix.de>
2018-07-12 20:01:11 +0200

    avoid name conflicts with std::log
    Signed-off-by: Cristian Kubis <cristian.kubis@tsunix.de>

--- src/kern.h
+++ src/kern.h
@@ -5,14 +5,14 @@
 #include "dispatch.h"
 
 namespace kern {
-    #define log(lvl, ...) Dispatch::get_logger()->write(lvl, __FILE__, __func__, __LINE__, __VA_ARGS__)
+    #define _log(lvl, ...) Dispatch::get_logger()->write(lvl, __FILE__, __func__, __LINE__, __VA_ARGS__)
 
-    #define trace(...) log(LogLevel::Trace, __VA_ARGS__)
-    #define debug(...) log(LogLevel::Debug, __VA_ARGS__)
-    #define info(...) log(LogLevel::Info, __VA_ARGS__)
-    #define warning(...) log(LogLevel::Warning, __VA_ARGS__)
-    #define error(...) log(LogLevel::Error, __VA_ARGS__)
-    #define fatal(...) log(LogLevel::Fatal, __VA_ARGS__)
+    #define trace(...) _log(LogLevel::Trace, __VA_ARGS__)
+    #define debug(...) _log(LogLevel::Debug, __VA_ARGS__)
+    #define info(...) _log(LogLevel::Info, __VA_ARGS__)
+    #define warning(...) _log(LogLevel::Warning, __VA_ARGS__)
+    #define error(...) _log(LogLevel::Error, __VA_ARGS__)
+    #define fatal(...) _log(LogLevel::Fatal, __VA_ARGS__)
 }
 
 #endif //LOGGER_H
