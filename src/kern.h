d2f7056126e0e323e27bc174ba4e3c5bc95ec514
Cristian Kubis <cristian.kubis@tsunix.de>
2019-01-20 09:06:08 +0100

    fix namespace issues with the public interface
    Signed-off-by: Cristian Kubis <cristian.kubis@tsunix.de>

--- src/kern.h
+++ src/kern.h
@@ -7,12 +7,12 @@
 namespace kern {
     #define _log(lvl, ...) Dispatch::get_logger()->write(lvl, __FILE__, __func__, __LINE__, __VA_ARGS__)
 
-    #define trace(...) _log(LogLevel::Trace, __VA_ARGS__)
-    #define debug(...) _log(LogLevel::Debug, __VA_ARGS__)
-    #define info(...) _log(LogLevel::Info, __VA_ARGS__)
-    #define warning(...) _log(LogLevel::Warning, __VA_ARGS__)
-    #define error(...) _log(LogLevel::Error, __VA_ARGS__)
-    #define fatal(...) _log(LogLevel::Fatal, __VA_ARGS__)
+    #define trace(...) kern::_log(kern::LogLevel::Trace, __VA_ARGS__)
+    #define debug(...) kern::_log(kern::LogLevel::Debug, __VA_ARGS__)
+    #define info(...) kern::_log(kern::LogLevel::Info, __VA_ARGS__)
+    #define warning(...) kern::_log(kern::LogLevel::Warning, __VA_ARGS__)
+    #define error(...) kern::_log(kern::LogLevel::Error, __VA_ARGS__)
+    #define fatal(...) kern::_log(kern::LogLevel::Fatal, __VA_ARGS__)
 }
 
 #endif //LOGGER_H
