66daa81e9b6173a25d13153b4b434e6dd9ee0884
Cristian Kubis <cristian.kubis@tsunix.de>
2019-03-23 15:30:12 +0100

    Add FileSink unit test
    Signed-off-by: Cristian Kubis <cristian.kubis@tsunix.de>

--- tests/testsink.cpp
+++ tests/testsink.cpp
@@ -1,6 +1,7 @@
-#include "sink.h"
+#include "kern.h"
 
 #include <cstdio>
+#include <cstring>
 #include <stdio.h>
 
 #include "catch.hpp"
@@ -45,6 +46,32 @@ TEST_CASE("FileSink creates file") {
     std::ifstream f(tmp_filename);
 
     CHECK( f.good() );
+
+    // remove temporary
+    remove(tmp_filename);
+}
+
+TEST_CASE("FileSink outputs to the given file") {
+    const char *res = "foobar";
+    char tmp_filename[] = "/tmp/kern_testing_XXXXXX";
+
+    // create tmp file name by creating a temp file and deleting it
+    // immediately. Using tmpnam would safe this additional step but
+    // it creates compiler warnings
+    REQUIRE( mkstemp(tmp_filename) != -1 );
+
+    auto sink = new FileSink(tmp_filename);
+    std::ifstream f(tmp_filename);
+
+    REQUIRE( f.good() );
+
+    sink->write("foobar");
+
+    char buf[16];
+    f.getline(buf, 16);
+    CHECK( strncmp(buf, res, strlen(res)) == 0 );
+
+    delete sink;
 
     // remove temporary
     remove(tmp_filename);
